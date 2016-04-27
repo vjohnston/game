42e71f9e86ee50bba718da2fb887e3106f654923
Victoria Johnston <1victoriajohnston@gmail.com>
2016-04-27 16:50:40 -0400

    adding game server file

---
+++ gameserver.py
@@ -0,0 +1,23 @@
+from twisted.internet import reactor
+from twisted.internet.protocol import Protocol
+from twisted.internet.protocol import Factory
+import pygame
+import sys
+import math
+from pygame.locals import *
+
+PLAYER_ONE_PORT = 32015
+PLAYER_TWO_PORT = 32016
+
+class PlayerConnection(Protocol):
+	def connectionMade(self):
+		print "player 1 connected"
+		
+	
+class PlayerConnectionFactory(Factory):
+	def buildProtocol(self, addr):
+		return PlayerConnection()
+
+if __name__ == '__main__':
+	reactor.listenTCP(PLAYER_ONE_PORT, PlayerConnectionFactory())
+	reactor.run()
